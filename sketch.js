0c6c4f54323f8d1831988c3203b597a36edc42f6
melshh <7000409@spsd.sk.ca>
2024-09-12 12:43:30 -0600

    123

---
+++ sketch.js
@@ -0,0 +1,31 @@
+// Cordinate systems
+// melsh.capalot
+// 09/11/24
+//Looking at how coordinares work in p5.js
+
+
+
+function setup() {
+  print("Setup runs once,at the star.")
+  createCanvas(500, 400);
+}
+
+function draw(){
+//runs over and over (targeting 60fp)
+//when possibles, keep draw() short
+background (220);
+drawCircles();
+
+
+
+}
+function drawCircles() {
+  //draw some (5) circles
+  fill(100,200,205);  //R, G, B
+  circle(0,0,50); 
+
+  fill(220,102,0);
+  circle(100,-20,50);
+
+  circle (500,)
+}
